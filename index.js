e38c723287ad2ebce0883d1a4fc46e157be99e08
Juan Carlos Lopez Larco <Jclopezlarco@gmail.com>
2023-08-09 18:04:32 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let array1 = ["Guadalupe","Ollie","Aki"];
+
+function writeCards() {
+    let array2 = [];
+    for (let i=0; i < array1.length; i++) {
+        array2.push("Thank you, " + array1[i] + ", for the wonderful surprise gift!")
+    }
+    return array2;
+}
+
+function countDown() {
+    for (let i2 = 0; i2 < 11; i2++ ) {
+    console.log (i2);
+}
+}
